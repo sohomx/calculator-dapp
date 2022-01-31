89608c58768a6ced813e537fde0050093ef1594c
sohom <sohom377@gmail.com>
2022-01-31 09:56:49 +0530

    various functions defined

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -16,22 +16,63 @@ pub mod myepicproject {
     }
 
     pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
-
+        let Calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        Ok(())
     }
 
     pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
-
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1*num2;
+        Ok(())
     }
 
     pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
-
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok(())
     }
 
     pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
-
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        calculator.remainder = num1 % num2;
+        Ok(())
     }
 
 }
 
 // #[derive(Accounts)]
 // pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub Calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    #[account(mut)]
+    pub Calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Substraction<'info> {
+    #[account(mut)]
+    pub Calculator: Account<'info, Calculator>
+}
+
+#[derive(Accounts)]
+pub struct Division<'info> {
+    #[account(mut)]
+    pub Calculator: Account<'info, Calculator>
+}
+
+
+#[account]
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i64,
+    pub reminder: i64   
+}
